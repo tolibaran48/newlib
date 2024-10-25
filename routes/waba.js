d247ce94ec65ddfdfb068e3a3c4d2a8e0524bc22
tolibaran48 <64205180+tolibaran48@users.noreply.github.com>
2024-10-25 14:27:14 +0300

    Update waba.js

--- routes/waba.js
+++ routes/waba.js
@@ -186,7 +186,7 @@ router.post("/", async (req, res) => {
         case "list_reply":
           switch (message.message.interactive.list_reply.id) {
             case "START_muhasebe_fatura":
-              console.log({ "from": message.message.from })
+              START_muhasebe_fatura(message.message.from)
               break;
           }
 
