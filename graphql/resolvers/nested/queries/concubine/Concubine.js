bb3fd0af2fd51592ceec46f62425d1b52fc3c82f
tolibaran48 <64205180+tolibaran48@users.noreply.github.com>
2024-10-25 19:03:17 +0300

    Update Concubine.js

--- graphql/resolvers/nested/queries/concubine/Concubine.js
+++ graphql/resolvers/nested/queries/concubine/Concubine.js
@@ -6,8 +6,7 @@ const Concubine = {
         await auth(token);
 
         try {
-            console.log(args)
-            return await Firma.findById(args.company);
+            return await Firma.findById(parent.company);
         } catch (error) {
             throw new GraphQLError(error)
         }
