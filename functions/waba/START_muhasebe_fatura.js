faf383a096f31e764aff834411d4c3e817d1dd92
tolibaran48 <64205180+tolibaran48@users.noreply.github.com>
2024-10-26 21:03:34 +0300

    Update START_muhasebe_fatura.js

--- functions/waba/START_muhasebe_fatura.js
+++ functions/waba/START_muhasebe_fatura.js
@@ -4,10 +4,10 @@ const WabaYetkili = require("../../models/WabaUser");
 const createToken = require("../../helpers/token");
 
 const START_muhasebe_fatura = async (phoneNumber) => {
-    if(phoneNumber.length==12){
-        phoneNumber=phoneNumber.substring(2)
+    if (phoneNumber.length == 12) {
+        phoneNumber = phoneNumber.substring(2)
     }
-    const yetki = await WabaYetkili.find({phoneNumber})
+    const yetki = await WabaYetkili.find({ phoneNumber })
 
     if (!yetki) {
         throw new GraphQLError('Kullanıcı adı veya parola yanlıştır.', {
@@ -40,50 +40,79 @@ const START_muhasebe_fatura = async (phoneNumber) => {
            }`
         }
     })
-    
+
     const companies = invoices.data.data.wabaUser.companies.map((company) => {
         return { "id": company.vergi.vergiNumarasi, "title": company.name, "description": "" }
     })
 
-   axios({
-        "method": "POST",
-        "url": `https://graph.facebook.com/v18.0/${process.env.WABA_PHONE_ID}/messages`,
-        "headers": {
-            Authorization: `Bearer ${process.env.WABA_API_TOKEN}`,
-        },
-        "data": {
-            "recipient_type": "individual",
-            "messaging_product": "whatsapp",
-            "to": phoneNumber,
-            "type": "interactive",
-            "interactive": {
-                "type": "flow",
-                "header": {
-                    "type": "text",
-                    "text": "Flow message header"
-                },
-                "body": {
-                    "text": "Flow message body"
-                },
-                "footer": {
-                    "text": "Flow message footer"
-                },
-                "action": {
-                    "name": "flow",
-                    "parameters": {
-                        "flow_message_version": "3",
-                        "flow_token": "1088448959650327",
-                        "flow_name": "invoice_flow_v1",
-                        "flow_cta": "Fatura indir",
-                        "flow_action_payload": {
-                            "screen": "COMPANY",
-                            "data": { companies, phoneNumber }
+    if (companies.length > 0) {
+        axios({
+            "method": "POST",
+            "url": `https://graph.facebook.com/v18.0/${process.env.WABA_PHONE_ID}/messages`,
+            "headers": {
+                Authorization: `Bearer ${process.env.WABA_API_TOKEN}`,
+            },
+            "data": {
+                "recipient_type": "individual",
+                "messaging_product": "whatsapp",
+                "to": phoneNumber,
+                "type": "interactive",
+                "interactive": {
+                    "type": "flow",
+                    "header": {
+                        "type": "text",
+                        "text": "Flow message header"
+                    },
+                    "body": {
+                        "text": "Flow message body"
+                    },
+                    "footer": {
+                        "text": "Flow message footer"
+                    },
+                    "action": {
+                        "name": "flow",
+                        "parameters": {
+                            "flow_message_version": "3",
+                            "flow_token": "1088448959650327",
+                            "flow_name": "invoice_flow_v1",
+                            "flow_cta": "Fatura indir",
+                            "flow_action_payload": {
+                                "screen": "COMPANY",
+                                "data": { companies, phoneNumber }
+                            }
                         }
                     }
                 }
             }
-        }
-    });
+        });
+    }
+    else {
+        axios({
+            "method": "POST",
+            "url": `https://graph.facebook.com/v18.0/${process.env.WABA_PHONE_ID}/messages`,
+            "headers": {
+                Authorization: `Bearer ${process.env.WABA_API_TOKEN}`,
+            },
+            "data": {
+                "messaging_product": "whatsapp",
+                "recipient_type": "individual",
+                "to": "<WHATSAPP_USER_PHONE_NUMBER>",
+                "type": "text",
+                "text": {
+                    "header": {
+                        "type": "text",
+                        "text": "Yetkili olduğunuz firma bulunmamaktadır."
+                    },
+                    "body": {
+                        "text": phoneNumber
+                    },
+                    "footer": {
+                        "text": "Bodrum Yalıkavak İş Sağlığı ve Güvenliği Ltd. Şti."
+                    }
+                }
+            }
+        });
+    }
 }
 
 module.exports = START_muhasebe_fatura;
